1c39d220cb56cc2e354d9cd0d1237386b5fdfcfb
Genis Bardales <genis96@yahoo.com>
2021-02-06 19:35:09 -0500

    populating the seed sql

--- schema.sql
+++ schema.sql
@@ -6,25 +6,25 @@ USE employees_db;
 -- Create table for "department"
 CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
-  name VARCHAR(30) NOT NULL,
+  names VARCHAR(30) NULL,
   PRIMARY KEY (id)
 );
 
 -- Create table for "role"
-CREATE TABLE role (
+CREATE TABLE roles (
   id INT AUTO_INCREMENT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10.5) NOT NULL,
-  department_id INT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10.5) NULL,
+  department_id INT NULL,
   PRIMARY KEY(id)
 );
 
 -- Create table "employee"
 CREATE TABLE employee (
   id INT AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_id INT NULL,
   manager_id INT NULL,
   PRIMARY KEY(id)
 );
---
+++ seed.sql
@@ -0,0 +1,33 @@
+USE employees_db;
+
+-- DEPARTMENT - sales, engineering, legal, finance
+INSERT INTO department ()
+VALUES ();
+
+INSERT INTO department ()
+VALUES ();
+
+INSERT INTO department ()
+VALUES ();
+
+
+-- ROLES
+INSERT INTO roles ()
+VALUES ();
+
+INSERT INTO roles ()
+VALUES ();
+
+INSERT INTO roles ()
+VALUES ();
+
+
+-- EMPLOYEES
+INSERT INTO employee ()
+VALUES ();
+
+INSERT INTO employee ()
+VALUES ();
+
+INSERT INTO employee ()
+VALUES ();
