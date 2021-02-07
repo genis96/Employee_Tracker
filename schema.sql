0be66526b45ec4512be1edec1d044c9377794b29
Genis Bardales <genis96@yahoo.com>
2021-02-06 19:22:51 -0500

    edited the tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 DROP DATABASE IF EXISTS employees_db;
-
 CREATE DATABASE employees_db;
 
 USE employees_db;
@@ -8,34 +7,28 @@ USE employees_db;
 CREATE TABLE department (
   id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEY(id)
+  PRIMARY KEY (id)
 );
 
 -- Create table for "role"
-CREATE TABLE department (
+CREATE TABLE role (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NOT NULL,
-  salary VARCHAR(30) NOT NULL,
-  department_id VARCHAR(30) NOT NULL,
+  salary DECIMAL(10.5) NOT NULL,
+  department_id INT,
   PRIMARY KEY(id)
 );
 
--- Create the table "employees"
-CREATE TABLE employees (
+-- Create table "employee"
+CREATE TABLE employee (
   id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id VARCHAR(30) NOT NULL,
-  manager_id VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
   PRIMARY KEY(id)
 );
 
--- Inserted a set of records into the table
-INSERT INTO employees (employee)
-VALUES ("Genis");
-
-INSERT INTO employees (employee)
-VALUES ("Ashleen");
-
-
-SELECT * FROM employees;
+-- SELECT * FROM department;
+-- SELECT * FROM role;
+-- SELECT * FROM employee;
